6a9d3b634356e01883adda70221e489bd443052f
Akash Singh <mr.singhaksh@gmail.com>
2024-03-07 00:35:52 +0530

    chore:fix port

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
-const server = app.listen(5000, console.log(`Server started on PORT ${port}`));
+const server = app.listen(port, console.log(`Server started on PORT ${port}`));
 
 const io = new Server(server, {
   pingTimeout: 60000,
