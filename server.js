7bf2183b0e41a81b79418a247385685c66af2910
Akash Singh <mr.singhaksh@gmail.com>
2024-03-06 16:08:36 +0530

    feat:add soket clean up

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const port = process.env.PORT || 5000;
 const server = app.listen(5000, console.log(`Server started on PORT ${port}`));
 
 const io = new Server(server, {
-  pingTimeout: 60000 ,
+  pingTimeout: 60000,
   cors: {
     origin: "http://localhost:5173",
   },
@@ -44,6 +44,9 @@ io.on("connection", socket => {
     console.log(`User joined Room: ${room}`);
   });
 
+  socket.on("typing", room => socket.in(room).emit("typing"));
+  socket.on("stop typing", room => socket.in(room).emit("stop typing"));
+
   socket.on("new message", newMessageReceived => {
     let chat = newMessageReceived?.chat;
     if (!chat?.users) return console.log("chat.users not defined");
@@ -51,5 +54,10 @@ io.on("connection", socket => {
       if (user._id == newMessageReceived.sender._id) return;
       socket.in(user._id).emit("message received", newMessageReceived);
     });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
   });
 });
